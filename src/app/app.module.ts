dc207f9fabaecb7aa5d6ddf7cb632bd7159f97d2
Yoann Gauthier <yoann_dev@net-c.com>
2018-11-10 19:15:37 +0100

    Finalize module 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,14 @@ import {
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/nav-bar.component'
-import { ToastrService } from './common/toastr.service'
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
 import { CollapsibleWellComponent } from './common/collapsible-well.component';
+
+// declare let toastr:Toastr
+let toastr:Toastr = window['toastr'];
 
 @NgModule({
   imports: [
@@ -45,7 +48,7 @@ import { CollapsibleWellComponent } from './common/collapsible-well.component';
   ],
   providers: [
     EventService, 
-    ToastrService, 
+    { provide: TOASTR_TOKEN, useValue: toastr },
     EventRouteActivator,
     EventListResolver,
     AuthService,
