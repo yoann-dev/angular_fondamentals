20cf549c8c3888b8cd3aa1397e93203fbcbfa9fe
Yoann Gauthier <yoann_dev@net-c.com>
2018-11-11 11:42:50 +0100

    Finalize module 15

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -18,7 +18,7 @@ export class SessionListComponent implements OnChanges {
 
   }
 
-  ngOnChanges(filter) {
+  ngOnChanges() {
     if(this.sessions) {
       this.filterSessions(this.filterBy);
       this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
